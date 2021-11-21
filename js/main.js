0d062f4e5cb0aa93c0eb93a08a00357ce1555d94
Monde Nyawose <lionelnyawose@gmail.com>
2021-11-21 22:21:29 +0200

    update index slides

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// initialize swiper
+
+
+// images only
+
+const mainSwiper = new Swiper('.swiper-container',{
+	autoplay: {
+       delay: 5000,
+     }
+})
+
+
+
+
+
+
+
+// Only used for "manual" slide changes
+// mainSwiper.on("slideNextTransitionStart", function(){
+//     const video = document.querySelector("video");
+//     if(video.closest(".swiper-slide-active") !== null){
+//         video.play()
+//     }else{
+//         video.pause()
+//     }
+// })
